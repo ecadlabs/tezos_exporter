8fc9bcc3edce12f0aba5b6358314d63107d3619b
Julius Volz <julius.volz@gmail.com>
2018-10-28 18:53:19 +0100

    Many cleanups and simplifications
    Biggest changes are:
    
    - Change away from using "normal" instrumentation metrics to use
      ConstMetrics, as is recommended for exporters.
    - Remove complex run group lifecycle handling from main, as this is a simple
      exporter and can just be killed without requiring an orderly shutdown.

--- collector/network.go
+++ collector/network.go
@@ -2,73 +2,55 @@ package collector
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/ecadlabs/go-tezos"
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-// NetworkCollector collects metrics about a Tezos nodes network properties
-type NetworkCollector struct {
-	//	logger  log.Logger
-	errors  *prometheus.CounterVec
-	client  *tezos.Client
-	timeout time.Duration
+var (
+	sentBytesDesc = prometheus.NewDesc("tezos_node_sent_bytes_total", "Total number of bytes sent from this node.", nil, nil)
+	recvBytesDesc = prometheus.NewDesc("tezos_node_recv_bytes_total", "Total number of bytes received by this node.", nil, nil)
+)
 
-	nodeSentBytesTotal *prometheus.CounterVec
-	nodeRecvBytesTotal *prometheus.CounterVec
+// NetworkCollector collects metrics about a Tezos node's network properties.
+type NetworkCollector struct {
+	logger  log.Logger
+	errors  *prometheus.CounterVec
+	service tezos.NetworkService
+	timeout time.Duration
 }
 
-// NewNetworkCollector returns a new NewtworkCollector
-func NewNetworkCollector(errors *prometheus.CounterVec, client *tezos.Client, timeout time.Duration) *NetworkCollector {
+// NewNetworkCollector returns a new NetworkCollector.
+func NewNetworkCollector(logger log.Logger, errors *prometheus.CounterVec, service tezos.NetworkService, timeout time.Duration) *NetworkCollector {
 	errors.WithLabelValues("network").Add(0)
 
 	return &NetworkCollector{
-		//		logger:  logger,
+		logger:  logger,
 		errors:  errors,
-		client:  client,
+		service: service,
 		timeout: timeout,
-
-		nodeSentBytesTotal: prometheus.NewCounterVec(
-			prometheus.CounterOpts{
-				Name: "tezos_node_sent_bytes_total",
-				Help: "Total number of bytes sent from this node",
-			},
-			[]string{"identity"},
-		),
-		nodeRecvBytesTotal: prometheus.NewCounterVec(
-			prometheus.CounterOpts{
-				Name: "tezos_node_recv_bytes_total",
-				Help: "Total number of bytes recieved by this node",
-			},
-			[]string{"identity"},
-		),
 	}
 }
-func (c *NetworkCollector) Describe(ch chan<- *prometheus.Desc) {
 
-	c.nodeSentBytesTotal.Describe(ch)
-	c.nodeRecvBytesTotal.Describe(ch)
+// Describe implements prometheus.Collector.
+func (c *NetworkCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- sentBytesDesc
+	ch <- recvBytesDesc
 }
 
-// Collect is called by the Prometheus registry when collecting metrics.
+// Collect implements prometheus.Collector and is called by the Prometheus registry when collecting metrics.
 func (c *NetworkCollector) Collect(ch chan<- prometheus.Metric) {
 	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
 	defer cancel()
 
-	stats, err := c.client.Network.GetStats(ctx)
+	stats, err := c.service.GetStats(ctx)
 	if err != nil {
 		c.errors.WithLabelValues("network_stat").Add(1)
-		fmt.Print(err)
-		// level.Warn(c.logger).Log("msg", "can't query /network/stat", "err", err)
+		level.Warn(c.logger).Log("msg", "error querying /network/stat", "err", err)
 	}
-	c.nodeSentBytesTotal.WithLabelValues("identity").Add(float64(stats.TotalBytesSent))
-	c.nodeSentBytesTotal.Collect(ch)
-	c.nodeRecvBytesTotal.WithLabelValues("identity").Add(float64(stats.TotalBytesRecv))
-	c.nodeRecvBytesTotal.Collect(ch)
-	//ch <- prometheus.New
-	//get data from rpc
-	//{ "total_sent": "76511326", "total_recv": "393777480", "current_inflow": 844,
-	//  "current_outflow": 192 }
+	ch <- prometheus.MustNewConstMetric(sentBytesDesc, prometheus.CounterValue, float64(stats.TotalBytesSent))
+	ch <- prometheus.MustNewConstMetric(recvBytesDesc, prometheus.CounterValue, float64(stats.TotalBytesRecv))
 }
--- main.go
+++ main.go
@@ -1,106 +1,50 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"fmt"
 	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
 	tezos "github.com/ecadlabs/go-tezos"
 	"github.com/ecadlabs/tezos_exporter/collector"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
-	"github.com/oklog/oklog/pkg/group"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-	metricsAddr := flag.String("metrics-listen-addr", ":9489", "TCP address on which to serve Prometheus metrics")
-	tezosAddr := flag.String("tezos-rpc", "http://localhost:8732", "TCP address of tezos node to monitor")
-	_ = tezosAddr
+	metricsAddr := flag.String("metrics-listen-addr", ":9489", "TCP address on which to serve Prometheus metrics.")
+	tezosAddr := flag.String("tezos-node-url", "http://localhost:8732", "URL of Tezos node to monitor.")
 
 	flag.Parse()
 
 	w := log.NewSyncWriter(os.Stderr)
 	logger := log.NewLogfmtLogger(w)
 
-	// metrics := newTezosMetrics()
-	client, err := tezos.NewClient(nil, *tezosAddr)
+	client, err := tezos.NewRPCClient(nil, *tezosAddr)
 	if err != nil {
-		level.Error(logger).Log("msg", fmt.Sprintf("Can't initlize tezos client: %v", err))
+		level.Error(logger).Log("msg", "error initializing Tezos RPC client", "err", err)
+		os.Exit(1)
 	}
+	service := tezos.Service{Client: client}
 
 	errors := prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "tezos_rpc_errors_total",
-		Help: "The total number of errors per rpc metric",
+		Help: "The total number of errors per RPC metric collector.",
 	}, []string{"collector"})
 
-	timeout := time.Duration(600) * time.Millisecond
+	timeout := 600 * time.Millisecond
 
 	reg := prometheus.NewRegistry()
 	reg.Register(prometheus.NewProcessCollector(os.Getpid(), ""))
 	reg.Register(prometheus.NewGoCollector())
-	reg.Register(collector.NewNetworkCollector(errors, client, timeout))
-	// reg.Register(metrics)
+	reg.Register(collector.NewNetworkCollector(logger, errors, service, timeout))
 
-	ctx, cancel := context.WithCancel(context.Background())
-
-	var g group.Group
-	{
-		// SIGTERM termination signal handler
-		term := make(chan os.Signal)
-		signal.Notify(term, os.Interrupt, syscall.SIGTERM)
-		g.Add(
-			func() error {
-				select {
-				case <-term:
-					level.Warn(logger).Log("msg", "Recieved SIGTERM, exiting gracefully...")
-				case <-ctx.Done():
-					break
-				}
-				return nil
-			},
-			func(err error) {
-				cancel()
-			},
-		)
+	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
+	if err := http.ListenAndServe(*metricsAddr, nil); err != nil {
+		level.Error(logger).Log("msg", "error starting webserver", "err", err)
+		os.Exit(1)
 	}
-	{
-		// Prometheus metrics HTTP server handler.
-		g.Add(
-			func() error {
-				http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
-				s := http.Server{
-					Addr: *metricsAddr,
-				}
-				errCh := make(chan error)
-				go func() {
-					errCh <- s.ListenAndServe()
-				}()
-
-				select {
-				case err := <-errCh:
-					return fmt.Errorf("error serving Prometheus metrics: %v", err)
-				case <-ctx.Done():
-					level.Info(logger).Log("msg", "Shutting down metrics HTTP server...")
-					s.Shutdown(ctx)
-					return nil
-				}
-			},
-			func(err error) {
-				cancel()
-			},
-		)
-	}
-
-	if err := g.Run(); err != nil {
-		level.Error(logger).Log("msg", "Error running exporter", "err", err)
-	}
-	level.Info(logger).Log("msg", "Shutdown complete!")
-
 }
